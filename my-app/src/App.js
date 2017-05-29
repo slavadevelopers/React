e6a561a4026e89cedb4518a6323814a7677297ff
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-05-29 15:23:09 +0300

    page Hello world is complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Hello, world!</h1>
       </div>
     );
   }
