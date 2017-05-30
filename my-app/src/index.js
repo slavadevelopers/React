8f6898cc90a7d9a2cf4b30af5cadd213ae5afa71
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-05-30 19:00:06 +0300

    clock example on page Rendering Elements

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,3 +6,18 @@ import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('Rendering-Elements')
+    );
+}
+
+setInterval(tick, 1000);
