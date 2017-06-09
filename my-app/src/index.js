0c03b0c05dec3047831c843929f167c2571611d3
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-09 20:42:40 +0300

    Page is Handling Events complete

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -75,3 +75,62 @@ function tick2() {
 }
 
 tick2();
+
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
+class LoggingButton extends React.Component {
+    // This syntax ensures `this` is bound within handleClick.
+    // Warning: this is *experimental* syntax.
+    handleClick = () => {
+        console.log('this is:', this);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                Click me
+            </button>
+        );
+    }
+}
+
+function App3() {
+    return (
+        <div>
+            <Toggle />
+            <LoggingButton />
+        </div>
+    )
+}
+
+function HandlingEvents() {
+    ReactDOM.render(
+        <App3 />,
+        document.getElementById('HandlingEvents')
+    );
+}
+
+HandlingEvents()
