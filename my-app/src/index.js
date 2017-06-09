d1714816eb53185115358acd7f28c4563d48fdcd
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-09 17:21:32 +0300

    encapsulating clock

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -21,3 +21,22 @@ function tick() {
 }
 
 setInterval(tick, 1000);
+
+
+function Clock(props) {
+    return (
+        <div>
+            <h1>Hello, world! New clock - example State and Lifecycle</h1>
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+
+function tick2() {
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('State-page')
+    );
+}
+
+setInterval(tick2, 1000);
