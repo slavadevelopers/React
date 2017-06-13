32bb2832c81b1d5a01199d7a98f2f71442885f81
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-13 19:20:45 +0300

    page is Conditional Rendering complete

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -265,3 +265,48 @@ function RenderMailbox() {
 
 RenderMailbox();
 
+function WarningBanner(props) {
+    if (!props.warn) {
+        return null;
+    }
+
+    return (
+        <div className="warning">
+            Warning!
+        </div>
+    );
+}
+
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showWarning: true}
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+
+    handleToggleClick() {
+        this.setState(prevState => ({
+            showWarning: !prevState.showWarning
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <WarningBanner warn={this.state.showWarning} />
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning ? 'Hide' : 'Show'}
+                </button>
+            </div>
+        );
+    }
+}
+
+function RenderClassPage() {
+    ReactDOM.render(
+        <Page />,
+        document.getElementById('RenderClassPage')
+    );
+}
+
+RenderClassPage();
