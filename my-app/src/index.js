36832ec5012ca3d6d2858cfe1322998cd2d374d3
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-14 19:13:11 +0300

    Calculator boil

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -560,3 +560,45 @@ function RenderReservation() {
 }
 
 RenderReservation();
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>Вода кипит</p>
+    }
+     return <p>Вода не кипит</p>
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange} />
+                <BoilingVerdict
+                    celsius={parseFloat(temperature)} />
+            </fieldset>
+        );
+    }
+}
+
+function RenderCalculator() {
+    ReactDOM.render(
+        <Calculator />,
+        document.getElementById('Calculator')
+    );
+}
+
+RenderCalculator();
