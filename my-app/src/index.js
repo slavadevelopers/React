75c3f3c7fe032c3a2455226cc864c6ebd4c158c6
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-24 11:21:52 +0300

    Page is Composition vs Inheritance example #1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -662,3 +662,33 @@ function RenderCalculator() {
 }
 
 RenderCalculator();
+
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="yellow">
+            <h1 className="Dialog-title">
+                Welcome
+            </h1>
+            <p className="Dialog-message">
+                Thank you for visiting our spacecraft!
+            </p>
+        </FancyBorder>
+    );
+}
+
+function renderWelcomeDialog() {
+    ReactDOM.render(
+        <WelcomeDialog />,
+        document.getElementById('WelcomeDialog')
+    )
+}
+
+renderWelcomeDialog();
